9c57ee5cfca00c595f52fdda754ebee886ccc77c
dj1vs <icemcpe099@gmail.com>
2024-03-17 23:00:29 +0300

    edit shell scripts

--- sh/kafka_create_topic.sh
+++ sh/kafka_create_topic.sh
@@ -3,4 +3,3 @@
 cd /usr/share/kafka
 
 bin/kafka-topics.sh --create --topic megachat --bootstrap-server localhost:9092
-bin/kafka-configs.sh --bootstrap-server localhost:9092 -entity-type topics --entity-name megachat --alter --add-config retention.ms=10000
