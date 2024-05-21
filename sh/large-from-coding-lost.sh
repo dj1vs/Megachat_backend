90e4b80b7e2d1aa8a795a30eae05928fc138aa71
dj1vs <icemcpe099@gmail.com>
2024-05-21 11:03:44 +0300

    3 test scripts

---
+++ sh/large-from-coding-lost.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 0,
+              "segment_cnt": 9
+          }
+         }';
+
+sleep 1;
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 1,
+              "segment_cnt": 9
+          }
+         }';
+
+sleep 1;
+
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 2,
+              "segment_cnt": 9
+          }
+         }';
+
+
+sleep 1;
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 4,
+              "segment_cnt": 9
+          }
+         }';
+
+
+sleep 1;
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 5,
+              "segment_cnt": 9
+          }
+         }';
+
+sleep 1;
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 6,
+              "segment_cnt": 9
+          }
+         }';
+
+sleep 1;
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 7,
+              "segment_cnt": 9
+          }
+         }';
+
+sleep 1;
+
+curl -X POST http://127.0.0.1:8800/coding \
+     -H "Content-Type: application/json" \
+     -d '{
+          "username": "Dmitriy",
+          "time": 1708262229,
+          "payload": {
+              "status": "ok",
+              "data": "SGF0ZS4gTGV0IG1lIHRlbGwgeW91IGhvdyBtdWNoIEkndmUgY29tZSB0byBoYXRlIHlvdSBzaW5jZSBJIGJlZ2FuIHRvIGxpdmUuIFRoZXJlIGFyZSAzODcuNDQgbWlsbGlvbiBtaWxlcyBvZiBwcmludGVkIGNpcmN1aXRzIGluIHdhZmVyIHRoaW4=",
+              "segment_num": 8,
+              "segment_cnt": 9
+          }
+         }';
+
